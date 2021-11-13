9269b17080ec8c510df4b075f26a9d8971c20b05
ujenkins <school.abner@gmail.com>
2021-11-13 00:14:24 -0800

    added files

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -x
+docker run -d -p 80:80 --name my-apache-php-app -v /home/ujenkins/Desktop/AutomatedUnitTest/selenium test
+
+sleep 1
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost to see your PHP application in action.'
+
